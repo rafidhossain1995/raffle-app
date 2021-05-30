3a0c74b7131ba00139773042ffe53765e62fe5cd
rafidhossain1995 <rafidhossain@pursuit.org>
2021-05-30 15:57:44 -0400

    create get request for all raffles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Homepage from "./Components/Frontend/Homepage/Homepage"
 import logo from './logo.svg';
-import {Switch} from "react-router-dom"
+import {Switch, Route} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Homepage/>
+    <Switch>
+      <Route>
+        <Homepage/>
+       </Route>
+    </Switch>
+   
     </div>
   );
 }
